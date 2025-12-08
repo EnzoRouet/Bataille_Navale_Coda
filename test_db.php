92b22cfa9bae19c7d3dd1a11af6869b9e6638bd6
kenzotrindade <hackgz0101@gmail.com>
2025-12-08 14:05:38 +0100

    Privileges: DB_MDP

--- test_db.php
+++ test_db.php
@@ -3,7 +3,7 @@
 $host = '127.0.0.1';
 $dbname = 'bataille_navale';
 $user = 'root';
-$pass = '1808';
+$pass = '1234';
 
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
