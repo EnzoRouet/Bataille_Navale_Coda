c306adde33ca064e4f8bb170b0a35dcf7b764a29
Enzo Rouet <enzorouet04@gmail.com>
2025-12-08 14:07:42 +0100

    Ajout fonction placer simple

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-phpinfo();
+$matrice = array_fill(0, 10, array_fill(0, 10, 0));
+
+
+function placer($grille, $taille){
+    for ($i = 0; $i < $taille; $i++){
+        $grille[$i][0] = $taille;
+    }
+    return $grille;
+}
+
+$matrice = placer($matrice, 3);
+var_dump($matrice);
 
 ?>
